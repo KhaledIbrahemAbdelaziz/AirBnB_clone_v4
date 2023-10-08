42433dba25cc6fe6bc0bca5692bbe93f92a101b1
Bigizic <olalekanisaac75@gmail.com>
2023-10-08 22:45:57 +0100

    Update 4-hbnb.js

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -14,7 +14,8 @@ $(document).ready(function () {
     }
     const amenHeader = $('div.amenities h4');
     if (Object.keys(amenityIds).length === 0) {
-      amenHeader.text('\u00A0');
+      amenHeader.text(&nbsp);
+    );
     } else {
       amenHeader.text(Object.keys(amenityIds).join(', '));
     }
